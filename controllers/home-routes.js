edcf0f4874f112baa2ecfc69da4fa1a4761539b3
Max W <mwinter@wesleyan.edu>
2022-01-17 16:45:23 -0600

    can view handlebars in web

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -34,3 +34,44 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
     });
 });
+
+// get a post by id
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'title', 'post_url', 'created_at'],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No Post found with this id' });
+            return;
+        }
+        res.json(dbPostData);
+
+        const post = dbPostData.get({ plain: true });
+
+        res.render('single-post', {post});
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
