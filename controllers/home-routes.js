d6403419d3b71b6c73962f0de307550a552dcef3
Max W <mwinter@wesleyan.edu>
2022-01-18 19:40:39 -0600

    added some changes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
 const sequelize = require('../config/connection');
-const e = require('express');
 
 // get all posts for the homepage
 router.get('/', (req, res) => {
