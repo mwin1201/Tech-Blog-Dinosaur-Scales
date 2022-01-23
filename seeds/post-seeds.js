489e9f1e9ded28be558baeef010140255eb9ae20
Max W <mwinter@wesleyan.edu>
2022-01-23 10:25:46 -0600

    added new links

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -3,22 +3,22 @@ const Post = require('../models/Post');
 const postData = [
     {
         title: 'Massive computer sale!',
-        post_url: 'www.apple.com',
+        post_url: 'https://www.microcenter.com/category/4294964325,518/computers',
         user_id: 2
     },
     {
         title: 'Formula 1 racing rocks',
-        post_url: 'www.formula1.com',
+        post_url: 'https://www.formula1.com/',
         user_id: 3
     },
     {
         title: 'All you need to know about the Premier League',
-        post_url: 'www.premierleague.com',
+        post_url: 'https://www.premierleague.com/matchweek/6684/blog',
         user_id: 1
     },
     {
         title: 'Why you should move to Minnesota',
-        post_url: 'www.google.com',
+        post_url: 'https://www.getbellhops.com/blog/moving-to-minnesota/',
         user_id: 2
     }
 ];
